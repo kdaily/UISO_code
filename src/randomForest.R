b3b229a877725e9a48f977f399b672349ae0a043
Kenny Daily <kmdaily@gmail.com>
2013-07-15 16:50:56 -0400

    Added random forest src

---
+++ src/randomForest.R
@@ -0,0 +1,53 @@
+## Do random forest
+set.seed(1)
+dataset <- eset.original.filter
+
+# Predict on cell lines
+samplenames.predict <- c("MC01", "Mkl-1_1", "Mkl-1_2", "Mkl-1_3", "Mkl-1_4", "Mkl-1_5", "Mkl-1_6",
+                         "Waga_1", "Waga_2", "Waga_3", "Waga_4", "Waga_5", "Waga_6",
+                         "UISO_late_1", "UISO_late_2", "UISO_late_3", "UISO_late_4", "UISO_late_5", "UISO_late_6",
+                         "UISO_early_1", "UISO_early_2", "UISO_early_3")
+
+# Train on tumor samples
+samplenames.train <- sampleNames(dataset)[!(sampleNames(dataset) %in% samplenames.predict)]
+
+dataset.train <- dataset[, as.character(samplenames.train)]
+edata.train <- t(exprs(dataset.train))
+classes.train <- pData(dataset.train)$cancertype
+
+## Use cell lines for prediction
+dataset.predict <- dataset[, samplenames.predict]
+
+## esetSel.celllines <- eset.batcheffect[as.character(topgenes.batcheffect.nocelllines$ID), as.character(pdata.celllines$sample)]
+## edata.test <- t(exprs(esetSel.celllines))
+
+## Use balanced sample size with the size of the smaller sample
+sample.size <- c(8,8)
+
+rf.eset <- randomForest(x=edata.train, y=classes.train, ntree=1000, ## mtry=10,
+                        sampsize=sample.size, replace=TRUE, strata=classes.train,
+                        importance=FALSE, proximity=TRUE, keep.trees=FALSE, keep.inbag=TRUE,
+                        do.trace=100)
+
+rf.predict <- cbind(as.data.frame(predict(rf.eset, t(exprs(dataset.predict)), type="prob")),
+                    data.frame(sample=factor(rep(c(0:4), c(1,6,6,6,3)),
+                                             label=c("MC01", "Mkl-1", "WaGa", "UISO_late", "UISO_early"))))
+
+# Unsupervised with all data, including cell lines
+# to visualize the proximity matrix with MDS
+edata.unsupervised <- t(exprs(dataset))
+classes.train.unsupervised <- pData(dataset)$cancertype
+samplenames.train.unsupervised <- sampleNames(dataset)
+
+rf.eset.unsupervised <- randomForest(x=edata.unsupervised, ntree=1000,
+                                     importance=FALSE, proximity=TRUE,
+                                     keep.trees=FALSE, keep.inbag=FALSE,
+                                     do.trace=100)
+
+ProjectTemplate::cache("rf.eset")
+ProjectTemplate::cache("rf.predict")
+ProjectTemplate::cache("rf.eset.unsupervised")
+ProjectTemplate::cache("samplenames.train")
+ProjectTemplate::cache("samplenames.train.unsupervised")
+ProjectTemplate::cache("classes.train")
+ProjectTemplate::cache("classes.train.unsupervised")
