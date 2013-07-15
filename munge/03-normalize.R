390868697062a71627e9f77891fc018cb5b90330
Kenny Daily <kmdaily@gmail.com>
2013-07-15 12:20:13 -0400

    Added normalization munging step

---
+++ munge/03-normalize.R
@@ -0,0 +1,14 @@
+## Normalize
+eset.original <- rma(data.cel)
+
+# Override the feature data with more informative gene names
+# The data is in data/fdata.hgu133a2.Rdata
+fData(eset.original) <- fdata.hgu133a2
+
+ProjectTemplate::cache("eset.original")
+
+# Remove the bottom 40% of probesets by variance
+original.nsfilter <- nsFilter(eset.original, remove.dupEntrez=FALSE, require.entrez=FALSE, var.cutoff=0.4)
+eset.original.filter <- original.nsfilter$eset
+
+ProjectTemplate::cache("eset.original.filter")
