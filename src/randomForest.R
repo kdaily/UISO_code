dbb0ab2c002acb944ecd31d6120a3c938de2d34f
Kenny Daily <kmdaily@gmail.com>
2013-07-16 16:01:59 -0400

    Updated random forest

--- src/randomForest.R
+++ src/randomForest.R
@@ -1,12 +1,13 @@
-## Do random forest
+## Train random forest on MCC and SCLC tumor samples
+## Apply to cell lines
+
 set.seed(1)
 dataset <- eset.original.filter
 
 # Predict on cell lines
 samplenames.predict <- c("MC01", "Mkl-1_1", "Mkl-1_2", "Mkl-1_3", "Mkl-1_4", "Mkl-1_5", "Mkl-1_6",
                          "Waga_1", "Waga_2", "Waga_3", "Waga_4", "Waga_5", "Waga_6",
-                         "UISO_late_1", "UISO_late_2", "UISO_late_3", "UISO_late_4", "UISO_late_5", "UISO_late_6",
-                         "UISO_early_1", "UISO_early_2", "UISO_early_3")
+                         "UISO_1", "UISO_2", "UISO_3", "UISO_4", "UISO_5", "UISO_6")
 
 # Train on tumor samples
 samplenames.train <- sampleNames(dataset)[!(sampleNames(dataset) %in% samplenames.predict)]
@@ -18,9 +19,6 @@ classes.train <- pData(dataset.train)$cancertype
 ## Use cell lines for prediction
 dataset.predict <- dataset[, samplenames.predict]
 
-## esetSel.celllines <- eset.batcheffect[as.character(topgenes.batcheffect.nocelllines$ID), as.character(pdata.celllines$sample)]
-## edata.test <- t(exprs(esetSel.celllines))
-
 ## Use balanced sample size with the size of the smaller sample
 sample.size <- c(8,8)
 
@@ -30,8 +28,7 @@ rf.eset <- randomForest(x=edata.train, y=classes.train, ntree=1000, ## mtry=10,
                         do.trace=100)
 
 rf.predict <- cbind(as.data.frame(predict(rf.eset, t(exprs(dataset.predict)), type="prob")),
-                    data.frame(sample=factor(rep(c(0:4), c(1,6,6,6,3)),
-                                             label=c("MC01", "Mkl-1", "WaGa", "UISO_late", "UISO_early"))))
+                    class=pData(dataset.predict)$class)
 
 # Unsupervised with all data, including cell lines
 # to visualize the proximity matrix with MDS
@@ -44,6 +41,7 @@ rf.eset.unsupervised <- randomForest(x=edata.unsupervised, ntree=1000,
                                      keep.trees=FALSE, keep.inbag=FALSE,
                                      do.trace=100)
 
+## Need to cache these to compile RMarkdown reports (in ./reports/)
 ProjectTemplate::cache("rf.eset")
 ProjectTemplate::cache("rf.predict")
 ProjectTemplate::cache("rf.eset.unsupervised")
