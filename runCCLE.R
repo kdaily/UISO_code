c0232fb30fcb91b7b7ca564c2dd1eaedd17cbabe
Kenny Daily <kmdaily@gmail.com>
2013-11-12 12:09:34 -0500

    Added munging helper for CCLE processing

---
+++ runCCLE.R
@@ -0,0 +1,10 @@
+# Process the CCLE data.
+# WARNING: The CCLE data processing requires a lot ($>20$GB)
+# of main memory to hold the expression data.
+# The batch processing step can take $>15$ hours.
+
+source("munge/CCLE/07-CCLE-getdata.R")
+source("munge/CCLE/08-CCLE-readcel.R")
+source("munge/CCLE/09-CCLE-normalize.R")
+source("munge/CCLE/10-CCLE-qc.R")
+source("munge/CCLE/11-CCLE-batcheffect.R")
