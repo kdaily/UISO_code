762f436f7310baf0f8791755dd0e15eb6e48ac7b
Kenny Daily <kmdaily@gmail.com>
2013-11-25 13:56:41 -0500

    Added devtools and dendextend

--- setup.R
+++ setup.R
@@ -3,7 +3,7 @@ required_libs <- c("ProjectTemplate", "reshape", "plyr", "ggplot2",
                    "knitr", "xtable", "grid", "affy", "annotate", "limma",
                    "hgu133a2.db", "hgu133plus2.db", "sva", "arrayQualityMetrics",
                    "affyQCReport", "snow", "cluster", "randomForest",
-                   "sigclust", "genefilter", "GSA", "venneuler", "GEOquery")
+                   "sigclust", "genefilter", "GSA", "venneuler", "GEOquery", "devtools")
 
 for (i in 1:length(required_libs)) {
   if (!(required_libs[i] %in% rownames(installed.packages()))) {
@@ -14,3 +14,9 @@ for (i in 1:length(required_libs)) {
 # Install BioConductor
 source("http://bioconductor.org/biocLite.R")
 biocLite()
+
+# Install dendextend from github
+require(devtools)
+install_github('dendextend', 'talgalili')
+require(Rcpp)
+install_github('dendextendRcpp', 'talgalili')
