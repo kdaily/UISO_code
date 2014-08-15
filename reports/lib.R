1b059fd268d5b129e8a2c4b9cd4e7d0db9604e6e
Kenny Daily <kmdaily@gmail.com>
2014-08-15 14:12:06 -0400

    Added library and helper functions.

---
+++ reports/lib.R
@@ -0,0 +1,25 @@
+# From http://gastonsanchez.wordpress.com/2012/08/27/scatterplot-matrices-with-ggplot/
+makePairs <- function(data)
+{
+  grid <- expand.grid(x = 1:ncol(data), y = 1:ncol(data))
+  grid <- subset(grid, x != y)
+  all <- do.call("rbind", lapply(1:nrow(grid), function(i) {
+    xcol <- grid[i, "x"]
+    ycol <- grid[i, "y"]
+    data.frame(xvar = names(data)[ycol], yvar = names(data)[xcol],
+               x = data[, xcol], y = data[, ycol], data)
+  }))
+  all$xvar <- factor(all$xvar, levels = names(data))
+  all$yvar <- factor(all$yvar, levels = names(data))
+  densities <- do.call("rbind", lapply(1:ncol(data), function(i) {
+    data.frame(xvar = names(data)[i], yvar = names(data)[i], x = data[, i])
+  }))
+  list(all=all, densities=densities)
+}
+
+# Get specific cols from phenodata of an ExpressionSet
+usefulpdata <- function(eset, cols=c("sample", "MCPyV.status", "classic", "class", "privsmet", "stage.code", "type")) {
+  
+  pData(eset)[, cols]
+  
+}
