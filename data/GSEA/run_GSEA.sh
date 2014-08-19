1533a7bc0b5669aa56e5d2f3fe371b6ee9721c5b
Kenny Daily <kmdaily@gmail.com>
2014-08-19 12:07:17 -0400

    Changed data dir, as well as no more classic vs variant comparison.

--- data/GSEA/run_GSEA.sh
+++ data/GSEA/run_GSEA.sh
@@ -4,13 +4,13 @@
 gsea_jar=/var/preserve/src/gsea2-2.0.13.jar
 
 # Output directory
-outputdir=./reports/GSEA/
+outputdir=./data/GSEA/
 
 # Standard CHIP file for all comparisons
 # Built from the variance filtered list of probesets/genes
 chipfile=./data/GSEA/tumor_classic_variant_filter.chip
 
-label=classic_vs_variant_filter
+label=tumor_vs_uiso_filter
 rankfile=./data/GSEA/${label}.rnk
 java -cp ${gsea_jar} xtools.gsea.GseaPreranked -gmx gseaftp.broadinstitute.org://pub/gsea/gene_sets/c2.cp.kegg.v3.1.symbols.gmt -collapse true -mode Max_probe -norm meandiv -nperm 1000 -rnk ${rankfile} -scoring_scheme weighted -rpt_label ${label} -chip ${chipfile} -include_only_symbols true -make_sets true -plot_top_x 50 -rnd_seed 1 -set_max 500 -set_min 15 -zip_report false -out ${outputdir} -gui false
 
